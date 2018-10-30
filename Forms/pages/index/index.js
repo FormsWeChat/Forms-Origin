4e9bad70f8d4d6b7a7628c6a6d307eca131c78fd
Shuang Wei <shwei@microsoft.com>
2018-10-30 12:59:18 +0800

    Test

--- Forms/pages/index/index.js
+++ Forms/pages/index/index.js
@@ -16,6 +16,9 @@ Page({
     })
   },
   onLoad: function () {
+    wx.navigateTo({
+      url: '../filter'
+    })
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
