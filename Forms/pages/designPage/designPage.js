722c13b7ec3989c066e9ff74c4680f7e0a868edb
Miaoting Feng <mifen@microsoft.com>
2018-10-31 21:09:24 +0800

    receive data from filter:wq

--- Forms/pages/designPage/designPage.js
+++ Forms/pages/designPage/designPage.js
@@ -3,7 +3,6 @@ Page({
     filters: [],
     loading: true,
     Title: "",
-    
     Options: [
       { Text: "Option1", Card: false, DoneInput: false }
     ],
@@ -11,7 +10,7 @@ Page({
   },
   //事件处理函数
 
-  
+
   onAddOptionsButton: function (e) {
     let newOptions = this.data.Options;
     newOptions.push({ Text: "Option" + (newOptions.length + 1), Card: false, DoneInput: false })
@@ -28,27 +27,32 @@ Page({
     if (e.detail.eventType === "button") {
       let newOptions = this.data.Options;
       let optionLength = newOptions.length;
-      newOptions[optionLength-1] = { Text:e.detail.item.title, Card: false, DoneInput: false }
+      newOptions[optionLength - 1] = { Text: e.detail.item.title, Card: false, DoneInput: false }
       this.setData({
         Options: newOptions,
         showBottom: "suggestion"
       })
     }
+    else if (e.detail.eventType === "suggestion") {
+      const component = this.selectComponent('#suggestion-List');
+      component.onLoad();
+    }
   },
 
-  onClickSuggestionTitle: function(e) {
+  onClickSuggestionTitle: function (e) {
     this.setData({
       Title: e.detail.Title,
       showBottom: "suggestion"
     });
   },
 
-  backToSuggestion: function(e) {
+  backToSuggestion: function (e) {
     console.log("Yeah:", e);
     this.setData({
       showBottom: e.detail.eventType
     });
-
+    const component = this.selectComponent('#suggestion-List');
+    component.onLoad();
   },
 
   onLoad: function () {
